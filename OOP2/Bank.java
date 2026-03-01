78cd46ec01c84e385bac924954832dc75d7644dd
Mansi Pambhar <mansipambhar28@gmail.com>
2026-03-01 22:22:12 +0530

    Create Bank.java

---
+++ OOP2/Bank.java
@@ -0,0 +1,22 @@
+package OOP2;
+
+public class Bank {
+    public static void main(String[] args) {
+
+        CurrentAccount cAcc =
+        new CurrentAccount("mansi", 1234567890,
+                        "123467mhgf", 0, 1100.00f, 0);
+
+        System.out.println(cAcc.acc_no);
+        System.out.println(cAcc.getUsername());
+        System.out.println(cAcc.getPass());
+        System.out.println("Balance: " + cAcc.getBalance());
+
+        cAcc.deposit(1000);
+        cAcc.withdraw(500);
+
+        System.out.println("Updated Balance: " + cAcc.getBalance());
+    }
+}
+
+ 
